5a624751fd587ff84e517a855ddef30fa1274909
Charles <738787105@qq.com>
2015-12-22 10:35:23 +0800

    初始化整理

--- FormKiwiCrawler.B/Program.cs
+++ FormKiwiCrawler.B/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace FormKiwiCrawler
+namespace FormKiwiCrawler.B
 {
     static class Program
     {
@@ -15,7 +15,7 @@ namespace FormKiwiCrawler
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());//frmAdd//
+            Application.Run(new Form1());
         }
     }
 }
